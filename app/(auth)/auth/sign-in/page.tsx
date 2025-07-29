625b0cb1abc0017c512df45bfd941ea86c798e16
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-07-29 21:55:59 +0530

    first commit

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import SignInFromClient from "@/featues/auth/components/SignInFromClient";
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <div className="space-y-6 flex flex-col items-center justify-center">
+      <Image src={"/logo.svg"} alt="logo" height={300} width={300} />
+      <SignInFromClient />
+    </div>
+  );
+};
+
+export default page;
